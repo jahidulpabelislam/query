b69ca1a236cfb20366d0da2cfaaca96fc20bb190
Jahidul Islam <jahidul@d3r.com>
2021-02-21 17:41:41 +0000

    Refactor Query to more of a builder

---
+++ src/Query/Builder.php
@@ -0,0 +1,269 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JPI\Database\Query;
+
+use JPI\Database;
+use JPI\Database\Collection;
+use JPI\Database\PaginatedCollection;
+
+/**
+ * Query builder. Allows building SQL queries also executing them and receiving in appropriate format.
+ *
+ * @author Jahidul Pabel Islam <me@jahidulpabelislam.com>
+ * @copyright 2012-2023 JPI
+ */
+class Builder {
+
+    /**
+     * @var Database
+     */
+    protected $database;
+
+    /**
+     * @var string|null
+     */
+    protected $table = null;
+
+    /**
+     * @var array
+     */
+    protected $columns = [];
+
+    /**
+     * @var array
+     */
+    protected $wheres = [];
+
+    /**
+     * @var array
+     */
+    protected $orderBys = [];
+
+    /**
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * @var int|null
+     */
+    protected $limit = null;
+
+    /**
+     * @var int|null
+     */
+    protected $page = null;
+
+    public function __construct(Database $database, string $table = null) {
+        $this->database = $database;
+        $this->table = $table;
+    }
+
+    protected function getGenerator(): Generator {
+        return new Generator($this);
+    }
+
+    /**
+     * @param $part string
+     * @return mixed
+     */
+    public function getPart(string $part) {
+        return $this->{$part} ?? null;
+    }
+
+    /**
+     * @param $key string
+     * @param $value string|int|float
+     * @return $this
+     */
+    public function param(string $key, $value): Builder {
+        $this->params[$key] = $value;
+        return $this;
+    }
+
+    public function params(array $params): Builder {
+        $this->params = array_merge($this->params, $params);
+        return $this;
+    }
+
+    public function table(string $table, string $alias = null): Builder {
+        $this->table = $alias ? "$table as $alias" : $table;
+        return $this;
+    }
+
+    public function column(string $column, string $alias = null): Builder {
+        if ($column === "*") {
+            $this->columns = [];
+        }
+        else {
+            $this->columns[] = $alias ? "$column as $alias" : $column;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $where string|int
+     * @return $this
+     */
+    public function where($where): Builder {
+        $expression = "=";
+
+        $args = func_get_args();
+
+        // params = int
+        if (!isset($args[1]) && is_numeric($where)) {
+            // (column, expression, value)
+            $args = ["id", "=", (int)$where];
+        }
+
+        // params = column, expression, value
+        if (isset($args[2])) {
+            [$where, $expression, $value] = $args;
+        }
+
+        // params = (column, expression, value) OR (column, value)
+        if (isset($args[1])) {
+            $value = $value ?? $args[1];
+
+            if (is_array($value)) {
+                $expression = 'IN';
+
+                $values = $value;
+                $ins = [];
+                foreach ($values as $i => $value) {
+                    $key = "{$where}_" . ($i + 1);
+                    $ins[] = ":$key";
+                    $this->param($key, $value);
+                }
+                $placeholder = "(" . implode(", ", $ins) . ")";
+            } else {
+                $this->param($where, $value);
+                $placeholder = ":$where";
+            }
+
+            $where = "$where $expression $placeholder";
+        }
+
+        $this->wheres[] = $where;
+        return $this;
+    }
+
+    public function orderBy(string $column, string $direction = "ASC"): Builder {
+        $this->orderBys[] = "$column $direction";
+        return $this;
+    }
+
+    public function page(int $page): Builder {
+        $this->page = $page;
+        return $this;
+    }
+
+    public function limit(int $limit, int $page = null): Builder {
+        if (!is_null($page)) {
+            $this->page($page);
+        }
+
+        $this->limit = $limit;
+        return $this;
+    }
+
+    public static function buildQuery(array $parts): string {
+        $query = implode("\n", $parts);
+        $query .= ";";
+
+        return $query;
+    }
+
+    public function createCollectionFromResult(array $rows) {
+        return new Collection($rows);
+    }
+
+    public function createPaginatedCollectionFromResult(array $rows, int $totalCount, int $limit, int $page) {
+        return new PaginatedCollection($rows, $totalCount, $limit, $page);
+    }
+
+    /**
+     * @return PaginatedCollection|Collection|array|null
+     */
+    public function select() {
+        $limit = $this->limit;
+
+        $query = static::buildQuery($this->getGenerator()->select());
+
+        if ($limit === 1) {
+            return $this->database->selectFirst($query, $this->params);
+        }
+
+        $rows = $this->database->selectAll($query, $this->params);
+
+        if (!$limit) {
+            return $this->createCollectionFromResult($rows);
+        }
+
+        $page = $this->page ?? 1;
+
+        $count = count($rows);
+
+        /**
+         * Do a DB query to get total count if:
+         *    - none found on a specific page than 1
+         *    - count is the limit
+         * Else we can work out the total
+         */
+        if ((!$count && $page > 1) || $count === $limit) {
+            // Replace the SELECT part in query with a simple count
+            $totalCount = (clone $this)->count();
+        }
+        else {
+            $totalCount = $limit * ($page - 1) + $count;
+        }
+
+        return $this->createPaginatedCollectionFromResult($rows, $totalCount, $limit, $page);
+    }
+
+    public function count(): int {
+        // Clear/reset
+        $this->columns = [];
+        $this->orderBys = [];
+
+        $this->column("COUNT(*)", "count");
+        $this->limit(1);
+
+        $row = $this->select();
+        return $row["count"] ?? 0;
+    }
+
+    public function insert(array $values): ?int {
+        $this->params($values);
+        $rowsAffected = $this->database->exec(
+            static::buildQuery($this->getGenerator()->insert($values)),
+            $this->params
+        );
+
+        if ($rowsAffected > 0) {
+            return null;
+        }
+
+        return $this->database->getLastInsertedId();
+    }
+
+    public function update(array $values): int {
+        $this->params($values);
+
+        return $this->database->exec(
+            static::buildQuery($this->getGenerator()->update($values)),
+            $this->params
+        );
+    }
+
+    public function delete(): int {
+        $rowsDeleted = $this->database->exec(
+            static::buildQuery($this->getGenerator()->delete()),
+            $this->params
+        );
+        return $rowsDeleted;
+    }
+}
---
+++ src/Query/Generator.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JPI\Database\Query;
+
+class Generator {
+
+    /**
+     * @var Builder
+     */
+    protected $builder;
+
+    public function __construct(Builder $builder) {
+        $this->builder = $builder;
+    }
+
+    /**
+     * Convenient function to pluck/get out the single value from an array if it's the only value.
+     * Then build a string value if an array.
+     *
+     * @param $value string[]|string|null
+     * @param $separator string
+     * @return string
+     */
+    protected static function arrayToString($value, string $separator = ","): string {
+        if (is_array($value) && count($value)) {
+            if (count($value) === 1) {
+                $value = array_shift($value);
+            }
+            else {
+                $separator .= "\n\t";
+                $value = "\n\t" . implode($separator, $value);
+            }
+        }
+
+        if (is_string($value)) {
+            return $value;
+        }
+
+        return "";
+    }
+
+    /**
+     * @param $part string
+     * @return mixed
+     */
+    protected function getPart(string $part) {
+        return $this->builder->getPart($part);
+    }
+
+    protected function generateWhereClause(): ?string {
+        $wheres = $this->getPart("wheres");
+        if (!$wheres) {
+            return null;
+        }
+
+        return "WHERE " . static::arrayToString($wheres, " AND ");
+    }
+
+    protected function generateOrderByClause(): ?string {
+        $orderBy = $this->getPart("orderBys");
+        if (!$orderBy) {
+            return null;
+        }
+
+        return "ORDER BY " . static::arrayToString($orderBy);
+    }
+
+    protected function generateLimitClause(): ?string {
+        $limit = $this->getPart("limit");
+        if (!$limit) {
+            return null;
+        }
+
+        $clause = "LIMIT $limit";
+
+        // Generate an offset, using limit & page values
+        $page = $this->getPart("page");
+        if ($page > 1) {
+            $offset = $limit * ($page - 1);
+            $clause .= " OFFSET $offset";
+        }
+
+        return $clause;
+    }
+
+    public function select(): array {
+        $columns = $this->getPart("columns");
+
+        $columns = count($columns) ? static::arrayToString($columns) : "*";
+
+        return array_filter([
+            "SELECT $columns",
+            "FROM {$this->getPart("table")}",
+            $this->generateWhereClause(),
+            $this->generateOrderByClause(),
+            $this->generateLimitClause(),
+        ]);
+    }
+
+    public function insert(array $values): array {
+        $sets = [];
+        foreach (array_keys($values) as $column) {
+            $sets[] = "$column = :$column";
+        }
+
+        return array_filter([
+            "INSERT INTO {$this->getPart("table")}",
+            "SET " . static::arrayToString($sets),
+        ]);
+    }
+
+    public function update(array $values): array {
+        $sets = [];
+        foreach (array_keys($values) as $column) {
+            $sets[] = "$column = :$column";
+        }
+
+        return array_filter([
+            "UPDATE {$this->getPart("table")}",
+            "SET " . static::arrayToString($sets),
+            $this->generateWhereClause()
+        ]);
+    }
+
+    public function delete(): array {
+        return array_filter([
+            "DELETE FROM {$this->getPart("table")}",
+            $this->generateWhereClause()
+        ]);
+    }
+}
