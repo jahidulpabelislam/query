7bc6a455cee058299430d538425eeab26fe1eb3c
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2023-03-11 19:42:15 +0000

    Rework classes for where conditions

--- src/Query/Where/Condition.php
+++ src/Query/Where/Condition.php
@@ -2,13 +2,19 @@
 
 declare(strict_types=1);
 
-namespace JPI\Database\Query;
+namespace JPI\Database\Query\Where;
 
-class WhereAnd implements WhereableInterface, ParamableInterface {
+use JPI\Database\Query\Builder;
+use JPI\Database\Query\Generator;
+use JPI\Database\Query\ParamableInterface;
+use JPI\Database\Query\WhereableInterface;
+use JPI\Database\Query\WhereableTrait;
+
+abstract class Condition implements WhereableInterface, ParamableInterface {
 
     use WhereableTrait;
 
-    protected $separator = "AND";
+    protected $condition;
 
     public function __construct(Builder $query) {
         $this->query = $query;
@@ -29,7 +35,11 @@ class WhereAnd implements WhereableInterface, ParamableInterface {
         return $this;
     }
 
+    public function getCondition(): string {
+        return $this->condition;
+    }
+
     public function __toString() {
-        return "(" . Generator::arrayToString($this->wheres, " $this->separator ") . ")";
+        return "(" . Generator::arrayToString($this->wheres, " {$this->getCondition()} ") . ")";
     }
 }
---
+++ src/Query/Where/OrCondition.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JPI\Database\Query\Where;
+
+class OrCondition extends Condition {
+
+    protected $condition = "OR";
+}
