2b42bf3d6464e1f257ab909d8fb8177c2bf466bb
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2024-04-25 22:19:03 +0100

    Implement ArrayAccess & Countable on ResultRow (#13)

--- src/Result/Row.php
+++ src/Result/Row.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace JPI\Database\Query\Result;
 
+use ArrayAccess;
 use ArrayIterator;
+use Countable;
 use JPI\Database\Query\ResultInterface;
 
-class Row implements ResultInterface {
+class Row implements ArrayAccess, Countable, ResultInterface  {
 
     public function __construct(
         protected array $data
@@ -18,12 +20,40 @@ class Row implements ResultInterface {
         return new static($row);
     }
 
+    protected function checkKey(string $key): void {
+        if (!array_key_exists($key, $this->data)) {
+            throw new \OutOfBoundsException("'$key' does not exist in the row.");
+        }
+    }
+
     public function getValue(string $key): mixed {
+        $this->checkKey($key);
         return $this->data[$key] ?? null;
+    }
+
+    public function offsetExists(mixed $key): bool {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function offsetGet(mixed $key): mixed {
+        return $this->getValue($key);
+    }
+
+    public function offsetSet(mixed $key, mixed $value): void {
+        $this->checkKey($key);
+        $this->data[$key] = $value;
+    }
+
+    public function offsetUnset(mixed $key): void {
+        unset($this->data[$key]);
     }
 
     public function toArray(): array {
         return $this->data;
+    }
+
+    public function count(): int {
+        return count($this->toArray());
     }
 
     public function getIterator(): ArrayIterator {
