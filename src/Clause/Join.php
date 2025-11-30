6beb1d78679afb96c062ac7789d9ce92a220aec6
Jahidul Pabel Islam <jahidul@d3r.com>
2025-11-30 10:54:04 +0000

    Tidy

--- src/Clause/Join.php
+++ src/Clause/Join.php
@@ -42,7 +42,7 @@ class Join extends AbstractClause {
     }
 
     public function getClause(): string {
-        return $this->clause . " $this->table ON";
+        return "$this->clause $this->table ON";
     }
 
     public function getItems(): array {
