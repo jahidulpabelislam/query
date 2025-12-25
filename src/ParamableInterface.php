ace60dc9fdf1a17d79f15ef1ed1963226a9f6848
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2025-12-25 15:18:45 +0000

    Support Stringable in place of pure string type (#27)

--- src/ParamableInterface.php
+++ src/ParamableInterface.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace JPI\Database\Query;
 
+use Stringable;
+
 interface ParamableInterface {
 
-    public function param(string $key, string|int|float $value): static;
+    public function param(string $key, Stringable|string|int|float $value): static;
 
     public function params(array $params): static;
 }
